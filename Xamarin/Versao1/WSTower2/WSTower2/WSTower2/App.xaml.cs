24d72cb5b4377c84fb8268d5c1b7e261b8e10022
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-07-14 17:50:57 -0300

    WSTowers-V1

---
+++ Xamarin/Versao1/WSTower2/WSTower2/WSTower2/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WSTower2.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WSTower2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new LoginView());
+            MainPage = new SplashView();
+            //MainPage = new LoginView();
+            //MainPage = new CadastroView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Xamarin/Versao1/WSTower2/WSTower2/WSTower2/View/SplashView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WSTower2.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashView : ContentPage
+    {
+        public SplashView()
+        {
+            InitializeComponent();
+        }
+    }
+}
